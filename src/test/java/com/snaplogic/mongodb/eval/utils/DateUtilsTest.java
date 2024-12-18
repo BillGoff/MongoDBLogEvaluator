d25143b383e123319de50076e79c51c5f22fe801
bgoff <bgoff@localhost>
2024-12-18 09:43:50 -0500

    updated

--- src/test/java/com/snaplogic/mongodb/eval/utils/DateUtilsTest.java
+++ src/test/java/com/snaplogic/mongodb/eval/utils/DateUtilsTest.java
@@ -16,40 +16,6 @@ import java.util.Date;
 class DateUtilsTest {
 
 	/**
-	 * Method to test getting the Start Date from the String supplied by the GUI.
-	 */
-	@Test
-	void testGetStartDateFromGuiString() 
-	{	
-		try
-		{
-			String guiDateString = "12/15/2024 00:00:00  - 12/18/2024 23:59:00";
-			Date expectedDate = DateUtils.toDate("12/15/2024 00:00:00");
-			Date startDate = DateUtils.getStartDateFromGuiString(guiDateString);
-			
-			if(expectedDate.equals(startDate))
-			{
-				System.out.println("Got the expected StartDate (" + DateUtils.toString(startDate) + 
-						": " + DateUtils.toString(expectedDate) + ")");
-			}
-			else
-			{	
-				StringBuilder sb = new StringBuilder("Did NOT get the expected start date from (" + guiDateString + ")]n");
-				sb.append("Expected (" + DateUtils.toString(expectedDate) + ")\n");
-				sb.append("But got (" + DateUtils.toString(startDate) + ")");
-				System.out.println(sb.toString());
-			}
-			
-			assertTrue(expectedDate.equals(startDate));
-		}
-		catch(ParseException pe)
-		{	
-			pe.printStackTrace();
-			fail("Failed to parse the GUI Date String into a start date!");
-		}
-	}
-	
-	/**
 	 * Method used to test the DateUtils method to get the expected start date.
 	 */
 	@Test
@@ -80,6 +46,40 @@ class DateUtilsTest {
 		{	
 			pe.printStackTrace();
 			fail("Failed to parse a date 3 days before date given!");
+		}
+	}
+	
+	/**
+	 * Method to test getting the Start Date from the String supplied by the GUI.
+	 */
+	@Test
+	void testGetStartDateFromGuiString() 
+	{	
+		try
+		{
+			String guiDateString = "12/15/2024 00:00:00  - 12/18/2024 23:59:00";
+			Date expectedDate = DateUtils.toDate("12/15/2024 00:00:00");
+			Date startDate = DateUtils.getStartDateFromGuiString(guiDateString);
+			
+			if(expectedDate.equals(startDate))
+			{
+				System.out.println("Got the expected StartDate (" + DateUtils.toString(startDate) + 
+						": " + DateUtils.toString(expectedDate) + ")");
+			}
+			else
+			{	
+				StringBuilder sb = new StringBuilder("Did NOT get the expected start date from (" + guiDateString + ")]n");
+				sb.append("Expected (" + DateUtils.toString(expectedDate) + ")\n");
+				sb.append("But got (" + DateUtils.toString(startDate) + ")");
+				System.out.println(sb.toString());
+			}
+			
+			assertTrue(expectedDate.equals(startDate));
+		}
+		catch(ParseException pe)
+		{	
+			pe.printStackTrace();
+			fail("Failed to parse the GUI Date String into a start date!");
 		}
 	}
 
